f3cb067b38a5a3a59e6dcc889d8b7ee8eb45f337
ferron <ferronrsmith@gmail.com>
2016-09-20 20:04:27 -0400

    fixed :bug: whereby the original logged object was being modified by the filter function

--- dist/log-ex-unobtrusive.js
+++ dist/log-ex-unobtrusive.js
@@ -332,25 +332,26 @@ angular.module("log.ex.uo", []).provider('logEx', ['$provide', function($provide
 
     /**
      * Evaluates an array of log arguments to be filtered using the provided or default filter keys
-     * @param {[] | Object} logArguments - array to be processed
+     * @param {[] | Object} logArguments - array/object to be processed
      * @returns {[] | Object} - returns a processed array with configured filter values replaced by filterString
      */
     var filterSensitiveValues = function(logArguments) {
-        if (isObjectOrArray(logArguments) && filterConfig.logFilters.length > 0) {
-            angular.forEach(logArguments, function(logValue, logKey) {
+        var obj = angular.copy(logArguments);
+        if (isObjectOrArray(obj) && filterConfig.logFilters.length > 0) {
+            angular.forEach(obj, function(logValue, logKey) {
                 angular.forEach(filterConfig.logFilters, function(filterValue) {
                     // replace filtered values here
                     if (itypeof(logValue) === 'object' &&
                         logValue.hasOwnProperty(filterValue) && !isObjectOrArray(logValue[filterValue])) {
                         logValue[filterValue] = filterConfig.filterString;
                     } else if (isObjectOrArray(logValue)) {
-                        logArguments[logKey] = filterSensitiveValues(logValue);
+                        obj[logKey] = filterSensitiveValues(logValue);
                     }
                 });
             });
-            return logArguments;
+            return obj;
         }
-        return logArguments;
+        return obj;
     };
 
     // Register $log decorator with AngularJS $provider
